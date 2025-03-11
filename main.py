4036740ab77a0e82c85d198736974c8070d0003d
Nicolai Dodeac <Mykola.Dodiak@gmail.com>
2025-03-11 12:38:58 +0000

    finish

--- main.py
+++ main.py
@@ -1,2 +1,97 @@
-print("Hello world!")
-print("give me green square")
+# fh = open('test.txt', 'w')
+# symbols_written = fh.write('hello!')
+# print(symbols_written) # 6
+# fh.close()
+
+# fh = open('test.txt', 'w+')
+# fh.write('hello!')
+# fh.seek(0)
+
+# first_two_symbols = fh.read(2)
+# print(first_two_symbols)  # 'he'
+
+# fh.close()
+
+# fh = open('test.txt', 'w')
+# fh.write('hello!')
+# fh.close()
+
+# fh = open('test.txt', 'r')
+# all_file = fh.read()
+# print(all_file)  # 'hello!'
+
+# fh.close()
+
+# fh = open('test.txt', 'w')
+# fh.write('hello!')
+# fh.close()
+
+# fh = open('test.txt', 'r')
+# while True:
+#     symbol = fh.read(1)
+#     if len(symbol) == 0:
+#         break
+#     print(symbol)
+
+# fh.close()
+
+# fh = open('test.txt', 'w')
+# fh.write('first line\nsecond line\nthird line')
+# fh.close()
+
+# fh = open('test.txt', 'r')
+# while True:
+#     line = fh.readline()
+#     if not line:
+#         break
+#     print(line)
+
+# fh.close()
+
+# fh = open('test.txt', 'w')
+# fh.write('first line\nsecond line\nthird line')
+# fh.close()
+
+# fh = open('test.txt', 'r')
+# lines = [el.strip() for el in fh.readlines()]
+# print(lines)
+
+# fh.close()
+
+# fh = open('test.txt', 'w+')
+# fh.write('hello!')
+
+# fh.seek(3)
+# second = fh.read()
+# print(second)  # 'e'
+
+# fh.close()
+
+# fh = open("test.txt", "w+")
+# fh.write("hello!")
+
+# position = fh.tell()
+# print(position)  # 6
+
+# fh.seek(4)
+# position = fh.tell()
+# print(position)  # 1
+
+# fh.read(4)
+# position = fh.tell()
+# print(position)  # 3
+
+# fh.close()
+
+
+# with open("test.txt", "w") as fh:
+#     fh.write("first line\nsecond line\nthird line")
+
+# with open("test.txt", "r") as fh:
+#     lines = [el.strip() for el in fh.readlines()]
+
+# print(lines)
+
+
+with open('raw_data.bin', 'wb') as fh:
+    fh.write(b'Hello world!')
